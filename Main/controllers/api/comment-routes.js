2906d6fd6ff358017c0914546c9ef78dbd2f1c33
Robert Nguyen <robertng.thien@gmail.com>
2023-09-17 11:45:10 -0700

    HW

---
+++ Main/controllers/api/comment-routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const { Comment } = require('../../models');
+
+
+// This will grab all comments
+router.get('/', (req, res) => {
+  Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// This will portray "This is the comment", and show user_id and post_id
+router.post('/', withAuth, (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id
+  })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+// This will delete the comment
+router.delete('/:id', withAuth, (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCommentData => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
---
+++ Main/models/Comment.js
@@ -0,0 +1,48 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+
+// Comment will inherit the methods of Model
+class Comment extends Model {}
+
+// Initialize Comment
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+
+module.exports = Comment;
